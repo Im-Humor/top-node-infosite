8fae45dbbba62efb1ad785eff4cb7775764a529f
Im-Humor <orangite@gmail.com>
2023-11-27 13:25:35 -0700

    implement working project solution

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 var http = require("node:http");
 var fs = require("node:fs");
-const url = require('node:url');
+const url = require("node:url");
 
 const hostname = "127.0.0.1";
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-	fs.readFile("index.html", (err, data) => {
-        if (err) throw err;
-        console.log(req.url);
-		res.statusCode = 200;
-        res.setHeader("Content-Type", "text/html");
-        res.write(data);
-		res.end();
-	});
+	if (req.url != "/favicon.ico") {
+		let myURL = req.url;
+		myURL = myURL.slice(1, myURL.length);
+		fs.readdir(".", (err, files) => {
+			if (err) throw err;
+			let myFiles = files;
+			myFiles = myFiles.filter((item) => item.includes(".htm"));
+			if (myFiles.includes(myURL)) {
+				fs.readFile(myURL, (err, data) => {
+					if (err) throw err;
+					res.statusCode = 200;
+					res.setHeader("Content-Type", "text/html");
+					res.write(data);
+					res.end();
+				});
+			} else {
+				fs.readFile("404.html", (err, data) => {
+					if (err) throw err;
+					res.statusCode = 200;
+					res.setHeader("Content-Type", "text/html");
+					res.write(data);
+					res.end();
+				});
+			}
+		});
+	}
 });
 
 server.listen(port, hostname, () => {
